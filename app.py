cc4cd48ee416c12d79a4da7efbce2b88f3c03663
tonmcg <tony@emdata.ai>
2024-11-25 08:20:44 -0500

    added PORT to app.py and updated API urls in index.html

--- app.py
+++ app.py
@@ -42,4 +42,5 @@ def fetch_ak_results():
     return data
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
